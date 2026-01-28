c82ab9175417688700e9d186f560322329d28a3f
Fredrik Löfgren <fr3drik.l@gmail.com>
2026-01-28 15:51:34 +0100

    feat: start page initial setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  experimental: {
+        fonts: [{
+            provider: fontProviders.google(),
+            name: "Unna",
+            cssVariable: "--font-unna",
+            weights: [ "400", "700" ],
+        },
+        {
+            provider: fontProviders.google(),
+            name: "Open Sans",
+            cssVariable: "--font-open-sans",
+            weights: [ "400", "700" ],
+        }]
+    }
+});
---
+++ src/content/config.ts
@@ -0,0 +1,10 @@
+import { defineCollection } from 'astro:content';
+import { cldAssetsLoader } from 'astro-cloudinary/loaders';
+
+export const collections = {
+  partners: defineCollection({
+    loader: cldAssetsLoader({
+      folder: 'Partners', // The folder in Cloudinary
+    })
+  }),
+};
