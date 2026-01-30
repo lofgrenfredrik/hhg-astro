a1485ca169a5535aab7849f946e0c14f2b712fc4
Fredrik Löfgren <fr3drik.l@gmail.com>
2026-01-30 20:27:15 +0100

    --no-verify

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,10 +1,10 @@
-import { defineCollection } from 'astro:content';
-import { cldAssetsLoader } from 'astro-cloudinary/loaders';
+import { defineCollection } from "astro:content";
+import { cldAssetsLoader } from "astro-cloudinary/loaders";
 
 export const collections = {
   partners: defineCollection({
     loader: cldAssetsLoader({
-      folder: 'Partners', // The folder in Cloudinary
-    })
+      folder: "Partners", // The folder in Cloudinary
+    }),
   }),
 };
