844cd35766ce9459931087fa08790536941b414b
Fredrik Löfgren <fr3drik.l@gmail.com>
2026-01-30 18:28:24 +0100

    feat: add csp

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,13 +13,31 @@ export default defineConfig({
             name: "Unna",
             cssVariable: "--font-unna",
             weights: [ "400", "700" ],
+            },
+            {
+                provider: fontProviders.google(),
+                name: "Open Sans",
+                cssVariable: "--font-open-sans",
+                weights: [ "400", "700" ],
+        }],
+        csp: {
+            directives: [
+                "default-src 'self'",
+                "img-src 'self' https://res.cloudinary.com",
+                "connect-src 'self'",
+                "frame-ancestors 'none'"
+            ],
+            styleDirective: {
+                resources: [
+                "'self'", "'unsafe-inline'", "'unsafe-eval'"
+                ]
+            },
+            scriptDirective: {
+                resources: [
+                "'self'", "'unsafe-inline'"
+                ]
+            }
         },
-        {
-            provider: fontProviders.google(),
-            name: "Open Sans",
-            cssVariable: "--font-open-sans",
-            weights: [ "400", "700" ],
-        }]
     },
     site: 'https://www.henxhospitality.com',
     integrations: [sitemap(), robotsTxt()]
